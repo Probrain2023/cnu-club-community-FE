74412911b92f90365eafab0c7856c21b75c8e58e
minseok cho <whalstjr3950@naver.com>
2023-03-21 01:04:11 +0900

    홈 다시 수정

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Button } from "react-bootstrap"; //2번
-
+import { motion } from 'framer-motion';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -43,6 +43,7 @@ const TopContainer = styled.div`
 const ContentsContainer = styled.div`
   margin-top: 20vh;
   /* background-color: tomato; */
+  padding: 0 2%;
   width: 100%;
   height: 180vh;
   display: grid;
@@ -51,37 +52,43 @@ const ContentsContainer = styled.div`
    /* overflow-y: auto; */
   @media (min-width: 0px) {
     grid-template-columns: 1fr;
-    
+    grid-template-rows: repeat(auto-fit ,83vw);
   }
-  @media (min-width: 576px) {
+  @media (min-width: 744px) {
     grid-template-columns: 1fr 1fr;
-    
+    grid-template-rows: repeat(auto-fit ,43vw);
   }
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: repeat(auto-fit ,27rem);
   }
   @media (min-width: 1400px) {
     grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: repeat(auto-fit ,27rem);
+  }
+  @media (min-width: 1600px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-rows: repeat(auto-fit ,27rem);
   }
 `
 
-const Contents = styled.div`
-  margin-top: 3vh;
+const Contents = styled(motion.div)`
+  /* margin-top: 2vh; */
   border: 1px solid black;
   border-radius: 5%;
   @media (min-width: 0px) {
-    width: 20rem;
-    height: 24rem;
+    width: 90vw;
+    height: 80vw;
     /* background-color: green; */
   }
-  @media (min-width: 576px) {
-    width: 20rem;
-    height: 24rem;
+  @media (min-width: 744px) {
+    width: 40vw;
+    height: 40vw;
     /* background-color: pink; */
     
   }
   @media (min-width: 992px) {
-    width: 20rem;
+    width: 19rem;
     height: 24rem;
     /* background-color: blue; */
   }
@@ -95,12 +102,13 @@ const Contents = styled.div`
   justify-content: space-around;
   cursor: pointer;
 
-  &:hover{
-    scale: 1.2;
-    /* background-color: black; */
-    /* color: white; */
-  }
+  
+    
 `
+const ContentsVariants = {
+  hover: {y: -10, transition: 1,  boxShadow: " 0 10px rgba(0,0,0,0.3)"},
+  init: {y: 0}
+}
 
 const ContentsTitle = styled.div`
   margin-top: 10%;
@@ -133,14 +141,14 @@ const ContentsLike = styled.div`
 `
 
 function Home() {
-  const contentsArray = Array.from({ length: 10 }, (_, index) => index);
+  const contentsArray = Array.from({ length: 15 }, (_, index) => index);
   return (
     <TopContainer>
       <ContentsContainer>
           
         {
           contentsArray.map((_, index) => (
-            <Contents key={index}>
+            <Contents key={index} variants={ContentsVariants} initial="init" whileHover="hover">
             <ContentsTitle>Title</ContentsTitle>
             <ContentsMain>sample sample sample sample sample sample sample
             sample sample sample sample sample sample sample
